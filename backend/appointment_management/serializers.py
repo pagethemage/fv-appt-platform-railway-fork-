e4dcc212bd570b85200f0a2f5f8d75db289bd483
Thanh Hai Pham <21453800@students.latrobe.edu.au>
2024-09-17 08:51:42 +1000

    Setup the views.py and serializer.py

--- backend/appointment_management/serializers.py
+++ backend/appointment_management/serializers.py
@@ -1,7 +1,127 @@
 from rest_framework import serializers
 from .models import Appointment
+from .models import Availability
+from .models import Club
+from .models import Match
+from .models import Notification
+from .models import Preference
+from .models import Referee
+from .models import Relative
+from .models import Venue
+from .models import AppointmentManagementAppointment
+from .models import AuthGroup
+from .models import AuthGroupPermissions
+from .models import AuthPermission
+from .models import AuthUser
+from .models import AuthUserGroups
+from .models import AuthUserUserPermissions
+from .models import DjangoAdminLog
+from .models import DjangoContentType
+from .models import DjangoMigrations
+from .models import DjangoSession
+from .models import Sysdiagrams
 
 class AppointmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Appointment
+        fields = ['appointment_id', 'referee', 'venue', 'match', 'distance', 'appointment_date', 'status']
+
+class AvailabilitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Availability
+        fields = ['referee', 'date', 'start_time', 'duration']
+
+class ClubSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Club
+        fields = ['club_id', 'club_name', 'home_venue', 'contact_name','contact_phone_number']
+
+class MatchSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Match
+        fields = ['match_id', 'referee', 'home_club', 'away_club', 'venue', 'match_data', 'level']
+
+class NotificationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Notification
+        fields = ['notification_id', 'referee', 'match', 'notification_type', 'date']
+
+class PreferenceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Preference
+        fields = ['referee', 'venue']
+
+class RefereeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Preference
+        fields = ['referee_id', 'first_name', 'last_name', 'age', 'location', 'email', 'phone_number', 'experience_years', 'level']
+
+class RelativeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Relative
+        fields = ['referee', 'club', 'relative_name', 'relationship', 'age']
+
+class VenueSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Venue
+        fields = ['venue_id', 'venue_name', 'capacity', 'location']
+
+class AppointmentManagementAppointmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AppointmentManagementAppointment
         fields = ['id', 'type', 'status', 'date', 'time', 'venue']
+
+class AuthGroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AuthGroup
+        fields = ['name']
+
+class AuthGroupPermissionsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AuthGroupPermissions
+        fields = ['id', 'group', 'permission']
+
+class AuthPermissionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AuthPermission
+        fields = ['name', 'content_type', 'codename']
+
+class AuthUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AuthUser
+        fields = ['password', 'last_login', 'is_superuser', 'username', 'first_name', 'last_name', 'email', 'is_staff', 'is_active', 'date_joined']
+
+class AuthUserGroupsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AuthUserGroups
+        fields = ['id', 'user', 'group']
+
+class AuthUserUserPermissionsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AuthUserUserPermissions
+        fields = ['id', 'user', 'permission']
+
+class DjangoAdminLogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DjangoAdminLog
+        fields = ['action_time', 'object_id', 'object_repr', 'action_flag', 'change_message', 'content_type', 'user']
+
+class DjangoContentTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DjangoContentType
+        fields = ['app_label', 'model']
+
+class DjangoMigrationsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DjangoMigrations
+        fields = ['id', 'app', 'name', 'applied']
+
+class DjangoSessionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DjangoSession
+        fields = ['session_key', 'session_data', 'expire_date']
+
+class SysdiagramsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Sysdiagrams
+        fields = ['name', 'principal_id', 'diagram_id', 'version', 'definition']
--- backend/appointment_management/views.py
+++ backend/appointment_management/views.py
@@ -1,9 +1,134 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework import response
 from .models import Appointment
+from .models import Availability
+from .models import Club
+from .models import Match
+from .models import Notification
+from .models import Preference
+from .models import Referee
+from .models import Relative
+from .models import Venue
+from .models import AppointmentManagementAppointment
+from .models import AuthGroup
+from .models import AuthGroupPermissions
+from .models import AuthPermission
+from .models import AuthUser
+from .models import AuthUserGroups
+from .models import AuthUserUserPermissions
+from .models import DjangoAdminLog
+from .models import DjangoContentType
+from .models import DjangoMigrations
+from .models import DjangoSession
+from .models import Sysdiagrams
 from .serializers import AppointmentSerializer
+from .serializers import AvailabilitySerializer
+from .serializers import ClubSerializer
+from .serializers import MatchSerializer
+from .serializers import NotificationSerializer
+from .serializers import PreferenceSerializer
+from .serializers import RefereeSerializer
+from .serializers import RelativeSerializer
+from .serializers import VenueSerializer
+from .serializers import AppointmentManagementAppointmentSerializer
+from .serializers import AuthGroupSerializer
+from .serializers import AuthGroupPermissionsSerializer
+from .serializers import AuthPermissionSerializer
+from .serializers import AuthUserSerializer
+from .serializers import AuthUserGroupsSerializer
+from .serializers import AuthUserUserPermissionsSerializer
+from .serializers import DjangoAdminLogSerializer
+from .serializers import DjangoContentTypeSerializer
+from .serializers import DjangoMigrationsSerializer
+from .serializers import DjangoSessionSerializer
+from .serializers import SysdiagramsSerializer
 
 # Create your views here.
 class AppointmentViewSet(viewsets.ModelViewSet):
     queryset = Appointment.objects.all()
     serializer_class = AppointmentSerializer
+
+    # def list(self, request):
+    #     serializer_class = AppointmentSerializer(self.queryset, many = True)
+    #     return response(serializer_class.data)
+
+class AvailabilityViewSet(viewsets.ModelViewSet):
+    queryset = Availability.objects.all()
+    serializer_class = AvailabilitySerializer
+
+class ClubViewSet(viewsets.ModelViewSet):
+    queryset = Club.objects.all()
+    serializer_class = ClubSerializer
+
+class MatchViewSet(viewsets.ModelViewSet):
+    queryset = Match.objects.all()
+    serializer_class = MatchSerializer
+
+class NotificationViewSet(viewsets.ModelViewSet):
+    queryset = Notification.objects.all()
+    serializer_class = NotificationSerializer
+
+class PreferenceViewSet(viewsets.ModelViewSet):
+    queryset = Preference.objects.all()
+    serializer_class = PreferenceSerializer
+
+class RefereeViewSet(viewsets.ModelViewSet):
+    queryset = Referee.objects.all()
+    serializer_class = RefereeSerializer
+
+class RelativeViewSet(viewsets.ModelViewSet):
+    queryset = Relative.objects.all()
+    serializer_class = RelativeSerializer
+
+class VenueViewSet(viewsets.ModelViewSet):
+    queryset = Venue.objects.all()
+    serializer_class = VenueSerializer
+
+class AppointmentManagementAppointmentViewSet(viewsets.ModelViewSet):
+    queryset = AppointmentManagementAppointment.objects.all()
+    serializer_class = AppointmentManagementAppointmentSerializer
+
+class AuthGroupViewSet(viewsets.ModelViewSet):
+    queryset = AuthGroup.objects.all()
+    serializer_class = AuthGroupSerializer
+
+class AuthGroupPermissionsViewSet(viewsets.ModelViewSet):
+    queryset = AuthGroupPermissions.objects.all()
+    serializer_class = AuthGroupPermissionsSerializer
+
+class AuthPermissionViewSet(viewsets.ModelViewSet):
+    queryset = AuthPermission.objects.all()
+    serializer_class = AuthPermissionSerializer
+
+class AuthUserViewSet(viewsets.ModelViewSet):
+    queryset = AuthUser.objects.all()
+    serializer_class = AuthUserSerializer
+
+class AuthUserGroupsViewSet(viewsets.ModelViewSet):
+    queryset = AuthUserGroups.objects.all()
+    serializer_class = AuthUserGroupsSerializer
+
+class AuthUserUserPermissionViewSet(viewsets.ModelViewSet):
+    queryset = AuthUserUserPermissions.objects.all()
+    serializer_class = AuthUserUserPermissionsSerializer
+
+class DjangoAdminLogViewSet(viewsets.ModelViewSet):
+    queryset = DjangoAdminLog.objects.all()
+    serializer_class = DjangoAdminLogSerializer
+
+class DjangoContentTypeViewSet(viewsets.ModelViewSet):
+    queryset = DjangoContentType.objects.all()
+    serializer_class = DjangoContentTypeSerializer
+
+class DjangoMigrationsViewSet(viewsets.ModelViewSet):
+    queryset = DjangoMigrations.objects.all()
+    serializer_class = DjangoMigrationsSerializer
+
+class DjangoSessionViewSet(viewsets.ModelViewSet):
+    queryset = DjangoSession.objects.all()
+    serializer_class = DjangoSessionSerializer
+
+class SysdiagramsViewSet(viewsets.ModelViewSet):
+    queryset = Sysdiagrams.objects.all()
+    serializer_class = SysdiagramsSerializer
